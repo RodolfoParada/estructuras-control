6e8cd282acf6bb744156c1bd7ed7b06e816febdf
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-11-11 17:16:43 -0300

    feat: funcionalidad estructura de control for of

---
+++ structucture-forof.js
@@ -0,0 +1,14 @@
+// Fon of
+
+// Ideal para arrays y strings
+let colores = ["rojo", "verde", "azul"];
+
+for (let color of colores) {
+  console.log(color);
+}
+
+// Para strings
+let palabra = "JavaScript";
+for (let letra of palabra) {
+  console.log(letra);
+}
