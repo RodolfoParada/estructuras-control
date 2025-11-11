7193022861d1f2dae074199db2c29c92e8672ed8
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-11-11 17:00:35 -0300

    feat: funcionalidad de la structura de control for

---
+++ structures-for.js
@@ -0,0 +1,12 @@
+//for - El Bucle Clásico
+
+// for básico
+for (let i = 0; i < 5; i++) {
+  console.log(`Iteración ${i}`);
+}
+
+// for con arrays
+let frutas = ["manzana", "banana", "naranja"];
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
